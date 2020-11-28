3072f3915d583566c6e614697e3f6c4371aa4fcd
Mateus Xavier <mat.xaviersantos@gmail.com>
2020-11-28 18:00:27 -0300

    io atualizado
    ui painho

--- project/app.py
+++ project/app.py
@@ -1,18 +1,28 @@
 
 from flask import Flask
 from flask.templating import render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 
+mensagem = []
 
 app = Flask(__name__)
-io = SocketIO(app)
+io = SocketIO(app)  
+
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
 
-@io.on('Mensagemzada')
+
+@io.on('MandarMensagemzada')
 def send_message_handler(msg):
     emit('pegarMensagem', msg, json=True)
 
+
+@io.on('Mensagemzada')
+def message_handler(msg):
+    send(mensagem)
+
+
 if __name__ == "__main__":
-    io.run(app, debug=True) 
+    io.run(app, debug=True)     
